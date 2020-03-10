664734e6a2abe3f06c21546ae445208f7a71fe74
Tu Nombre <you@example.com>
2020-03-10 16:51:44 +0100

    html

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const inputNumber= document.querySelector('#input-number');
+const button = document.querySelector('#button');
+const track = document.querySelector('#track');
+const attemps = document.querySelector('#attemps');
+
+function getRandomNumber(max) {
+  returnMath.ceil(Math.random() * max);
+  console.log(getRandomNumber)
+}
+
