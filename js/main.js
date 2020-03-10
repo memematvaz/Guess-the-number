300ac9af5ede4fe5fb5e7b8cc1051fce207667cd
Tu Nombre <you@example.com>
2020-03-10 18:00:59 +0100

    less attemps

--- js/main.js
+++ js/main.js
@@ -1,12 +1,56 @@
 'use strict';
 
-const inputNumber= document.querySelector('#input-number');
+const inputNumber = document.querySelector('#input-number');
 const button = document.querySelector('#button');
 const track = document.querySelector('#track');
 const attemps = document.querySelector('#attemps');
 
+
+
+
 function getRandomNumber(max) {
-  returnMath.ceil(Math.random() * max);
-  console.log(getRandomNumber)
+  return Math.ceil(Math.random() * max);
+
 }
 
+const myNum = getRandomNumber(100);
+console.log(myNum)
+
+
+
+
+function compareRandomNumber() {
+  if (inputNumber.value == myNum) {
+    track.innerHTML = 'Has ganado CAMPEONA!!!';
+  }
+  if (inputNumber.value > myNum) {
+    track.innerHTML = 'Demasiado alto'
+  }
+  if (inputNumber.value < myNum) {
+    track.innerHTML = 'Demasiado bajo'
+  } 
+  if (inputNumber.value >  100 ) {
+    track.innerHTML = 'El número debe estar entre 1 y 100'
+  }
+}
+
+
+
+
+function increaseAttemp() {
+
+  let attempsQuantity = parseInt(attemps.innerHTML);
+  attempsQuantity = attemps.value + 1;
+  attemps.innerHTML = productQuantity;
+
+
+
+
+}
+
+
+
+
+
+button.addEventListener('click', compareRandomNumber);
+button.addEventListener('click', increaseAttemp);
